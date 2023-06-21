217b8c7f0b3c2eaaa0abfe467833c2925b33a4aa
Enigma <chaosdelegation@gmail.com>
2023-06-21 16:21:50 +0800

    feat: add example for getting and posting data

--- client/js/example.js
+++ client/js/example.js
@@ -1,3 +1,29 @@
 window.onload = function () {
   console.log('This is Example page.');
+
+  // Setup buttons.
+  document.getElementById('post-user').onclick = function () {
+    const url = '/example/user';
+    const user = { name: 'alfa', age: 18, isVerified: true };
+    axios
+      .post(url, user)
+      .then((resp) => {
+        console.log(`POST ${url}: response:`, resp);
+      })
+      .catch((err) => {
+        console.error(`Failed to POST ${url}:`, err);
+      });
+  };
+
+  document.getElementById('get-users').onclick = function () {
+    const url = '/example/users';
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(`GET ${url}: response:`, resp);
+      })
+      .catch((err) => {
+        console.error(`Failed to GET ${url}:`, err);
+      });
+  };
 };
--- fastapi_app/routers/example.py
+++ fastapi_app/routers/example.py
@@ -2,12 +2,14 @@ import os
 
 import aiofiles
 from fastapi import APIRouter
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, Response
+from pydantic import BaseModel
 
 _WORKING_DIR = os.path.realpath(os.path.dirname(__file__))
 _EXAMPLE_HTML_PATH = os.path.realpath(
     os.path.join(_WORKING_DIR, "../../client/example.html"),
 )
+_users = []
 
 router = APIRouter(
     prefix="/example",
@@ -15,7 +17,53 @@ router = APIRouter(
 )
 
 
+def to_lower_camel(string: str) -> str:
+    return (
+        "".join(
+            [
+                s.capitalize() if i > 0 else s
+                for i, s in enumerate(string.split("_"))
+            ]
+        )
+    )
+
+
+class User(BaseModel):
+    name: str
+    age: int
+    is_verified: bool
+
+    class Config:
+        alias_generator = to_lower_camel
+
+        # This make User model accept constructing with snake case
+        # attributes.
+        # Without this, `User(name="alfa", age=14, is_verified=True)`
+        # will raise an error which says `isVerified` field is required.
+        allow_population_by_field_name = True
+
+
+class Result(BaseModel):
+    success: bool
+    msg: str | None
+
+
 @router.get("")
 async def example_page():
     async with aiofiles.open(_EXAMPLE_HTML_PATH, "r") as f:
         return HTMLResponse(content=await f.read(), status_code=200)
+
+
+@router.get("/users")
+async def get_users() -> list[User]:
+    return _users
+
+
+@router.post("/user")
+async def add_user(user: User, resp: Response) -> Result:
+    if next((u for u in _users if u.name == user.name), None) is None:
+        _users.append(user)
+        return Result(success=True, msg=None)
+    else:
+        resp.status_code = 409  # Conflict.
+        return Result(success=False, msg="User already exists.")
